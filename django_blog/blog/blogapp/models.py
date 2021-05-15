63505ab0eddd4c06953856a8ad8ba7cb89de1394
choihanna <hnc24@naver.com>
2021-05-15 15:56:00 +0900

    apps,urls,settings,views,models

---
+++ django_blog/blog/blogapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    body = models.TextField()
+
+def __str__(self):
+    return self.title
